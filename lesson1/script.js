a13e2a47057515779213ddcd3810a7be0e9d4122
DaniilShmerko <Dan4ikshmerko@yandex.ru>
2024-05-14 21:23:35 +1000

    сделал первый репозиторий

---
+++ lesson1/script.js
@@ -0,0 +1,43 @@
+// function AlertD() {
+//     alert( "Hello, world!" )        //code
+// }
+//                         // вызываем скрипт столько раз соклько нужнo
+// AlertD ()
+
+
+/*function A() {
+    ещё можно
+    так транскрибировать
+}*/ 
+
+// переменные объявляют через let но не через var
+// let one = 1;
+// let two = 2;
+// let sum = one + two              теперь в логах на странице есть в консоли число
+// console.log(one, two, sum)
+
+// const COLOR_RED = "#HEX";  объявление константы
+
+// let messege1 = "hello, world!"
+// let messege2 = 'hello, world!'
+// let messege3 = "hello, 'world!'"               // Виды кавычек
+// let messege4 = 'hello, "world"'
+// console.log(messege1)
+// console.log(messege2)
+// console.log(messege3)
+// console.log(messege4)
+// let name = prompt("КАК?")
+// let messege = `Are you ${name} ?`
+// console.log(messege)
+
+типы данных 
+String
+number
+Boolean
+null // ничего но есть ссылка на память
+undefined // ничего
+
+//          ** возвести в степень
+//          // целочисленное делеине
+//          || или
+//          && и
